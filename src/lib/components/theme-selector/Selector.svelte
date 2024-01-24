ac02531e341cdf39b0a4ab11262544658c6c9c55
Jamie Whelpton <jamie.whelpton94@gmail.com>
2024-01-24 08:59:30 +0000

    Add back theme selector

--- src/lib/components/theme-selector/Selector.svelte
+++ src/lib/components/theme-selector/Selector.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { Drawer, Trigger, Content, Header, Title, Footer } from '$lib/components/ui/drawer';
-	import { type Theme, themes } from '$lib/components/editor';
+	import { themes } from '$lib/components/editor';
 	import Card from './Card.svelte';
 	import { preferences, theme } from '$lib/stores/theme';
 
@@ -12,32 +12,37 @@
 </script>
 <Drawer>
 	<Trigger>Open</Trigger>
-	<Content>
-		<Header>
-			<Title>Theme</Title>
-		</Header>
-		<Footer>
+	<Content class="max-h-[96%]">
+		<div class="max-w-md w-full mx-auto flex flex-col overflow-auto p-4">
 			{$theme.name}
-			<div class="w-full flex gap-6 snap-x overflow-x-auto pb-14">
-				<div class="snap-center shrink-0">
-					<div class="shrink-0 w-4 sm:w-48"></div>
-				</div>
-				{#each themes as _theme}
-					{@const selected = $theme.name === _theme.name}
-					<div class="snap-center shrink-0 first:pl-8 last:pr-8">
-							<div on:click={() => preferences.set({theme: _theme.name})} class="shrink-0">
+<!--			<div class="w-full flex flex-col gap-6 snap-y">-->
+<!--				<div class="snap-center shrink-0">-->
+<!--					<div class="shrink-0 w-4 sm:w-48"></div>-->
+<!--				</div>-->
+<!--				{#each themes as _theme}-->
+<!--					{@const selected = $theme.name === _theme.name}-->
+<!--					<div class="snap-center shrink-0 first:pl-8 last:pr-8">-->
+<!--						<div on:click={() => preferences.set({theme: _theme.name})} class="shrink-0">-->
 
-									<Card {selected} theme={_theme}/>
+<!--							<Card {selected} theme={_theme}/>-->
 
 
-							</div>
-						</div>
-				{/each}
-				<div class="snap-center shrink-0">
-					<div class="shrink-0 w-4 sm:w-48"></div>
-				</div>
-			</div>
+<!--						</div>-->
+<!--					</div>-->
+<!--				{/each}-->
+<!--				<div class="snap-center shrink-0">-->
+<!--					<div class="shrink-0 w-4 sm:w-48"></div>-->
+<!--				</div>-->
+<!--			</div>-->
+		</div>
 
-		</Footer>
+
+<!--				<Header>-->
+<!--			<Title>Theme</Title>-->
+<!--		</Header>-->
+<!--		<Footer>-->
+
+
+<!--		</Footer>-->
 	</Content>
 </Drawer>
